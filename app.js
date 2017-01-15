c987e645a5ae00178f3f8bef41b6cd1a4c600746
Edmundo Pichardo <ce.pichardo@gmail.com>
2017-01-15 00:29:35 -0400

    Ahora tenemosl listo los eventos por marker

--- app.js
+++ app.js
@@ -3,17 +3,23 @@
     var options = mapster.MAP_OPTIONS;
     element = document.getElementById("map");
     map = Mapster.create(element,options);
-    // map.addMarker(19.450913,-70.694669);
-    map.addMarker(19.450913,-70.694669,true); //Hacerlo draggable
-    // var marker = new google.maps.Marker({
-    //     position: {
-    //         lat: 19.450913,
-    //         lng: -70.694669,
-    //     },
-    //     map: map.gMap,
-    //     icon: ''//icon url: mapicons.nicolasmoilet.com
-    // });
+    // map.addMarker(19.450913,-70.694669,true);
+//        Vamos a trabajr con los eventos del marker
+//    Vamos a hacero mas user friendly, que sepamos que estamos pasando a la funcion
+    map.addMarker({
+        lat:19.450913,
+        lng:-70.694669,
+        draggable:true,
+    //    Crearemos un event obj
+        event: {
+            name: 'dragend',
+            callback: function(){
+                console.log("dragend");
+            }
+        }
 
+    });
+//    Ahora que tenemos los custom options, vamos a pasar los custom events
 
 
 
